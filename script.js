9e03463369f3636eb8209cbf8ffd8ad4e114b149
Proyecto-Santiaggo <164711134+Proyecto-Santiaggo@users.noreply.github.com>
2025-06-23 00:06:44 -0300

    script.js

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const background = document.getElementById("code-background");
-  const words = [
-    "oblivion", "komi", "remind", "dosimedia", "1319", "verum",
-    "licenciado posting", "puro clown 1", "puro clown 2", "puro clown 3", 
-    "pinkeye", "tontos y asociados", "el server más zzz", "mencho rol", 
-    "illuminatyland", "sigmaworld"
-  ];
-  
-  function generateCodeLine() {
-    const codeStream = document.createElement("div");
-    codeStream.className = "code-stream";
+body {
+  margin: 0;
+  overflow: hidden;
+  background-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
-    let codeText = "";
-    for (let i = 0; i < 20; i++) { 
-      codeText += `<span class="glitch-word">${words[Math.floor(Math.random() * words.length)]}</span> `;
-    }
-
-    codeStream.innerHTML = codeText;
-    codeStream.style.top = `${Math.random() * -100}px`; // Posición vertical aleatoria
-    codeStream.style.left = `${Math.random() * 100}%`; // Posición horizontal aleatoria
-    background.appendChild(codeStream);
-
-    setTimeout(() => {
-      codeStream.remove();
-    }, 10000); 
-  }
-
-  setInterval(generateCodeLine, 100); 
-});
+canvas {
+  border: 3px solid white;
+  background: url('https://upload.wikimedia.org/wikipedia/commons/3/3a/Colosseum_in_Rome%2C_Italy_-_April_2007.jpg');
+  background-size: cover;
+}
